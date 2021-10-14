163fcb44363cc86bed8ff03a86edbf394984f0ae
Arya _alvin <aryaalvin9@gmail.com>
2021-10-15 05:57:05 +0700

    push video Player

--- flutter_play_apps/android/app/src/main/kotlin/com/example/flutter_play_apps/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_play_apps
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- player_test/lib/player_view.dart
+++ player_test/lib/player_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:flutter/rendering.dart';
+import 'package:rxdart/rxdart.dart';
 
 class VideoPlayerBuild extends StatelessWidget {
   VideoPlayerBuild({Key? key}) : super(key: key);
@@ -8,14 +11,28 @@ class VideoPlayerBuild extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      child: Align(
-        alignment: Alignment.center,
-        child: VideoPlayerView(),
-      ),
+    return ValueListenableBuilder<ThemeVariation>(
+      valueListenable: themeNotifier, 
+      builder: (context, value, child){
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch: value.color, brightness: value.brightness
+          ),
+          home: VideoPlayerView(),
+        );
+      }
     );
   }
+}
+
+var themeNotifier = ValueNotifier<ThemeVariation>(
+  const ThemeVariation(Colors.blue, Brightness.light),
+);
+
+class ThemeVariation{
+  const ThemeVariation(this.color, this.brightness);
+  final MaterialColor color;
+  final Brightness brightness;
 }
 
 class VideoPlayerView extends StatefulWidget {
@@ -29,87 +46,111 @@ class _VideoPlayerViewState extends State<VideoPlayerView> {
   
   late VideoPlayerController _controller;
 
+  late Future <void> _initialize;
+
+  var _isShowingWidgetOutline = false;
+  var _isFullWatch = false;
+
   @override
   void initState() {
+     _controller = VideoPlayerController.network("https://api.dacast.com/v2/vod/1140446/download?apikey=137797_ae2ee525d4a1ab988984&_format=JSON");
+     _initialize = _controller.initialize();
+     _controller.setLooping(false);
+     _controller.setVolume(1.0);
     super.initState();
-    _controller = VideoPlayerController.network("https://api.dacast.com/v2/vod/1140446/download?apikey=137797_ae2ee525d4a1ab988984&_format=JSON");
-    _controller
-      ..setLooping(true)
-      ..initialize()
-      ..setVolume(1.0)
-      ..addListener(() {
-        setState(() {
-          _controller.play();
-        });
-      });
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: Container(
-        constraints: BoxConstraints(
-          maxHeight: MediaQuery.of(context).size.height,
+    return Scaffold(
+      body: Column(
+        children: <Widget> [
+          FutureBuilder(
+            future: _initialize,
+            builder: (context, snapshot) {
+              if(snapshot.connectionState == ConnectionState.done){
+                return AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                );
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                ); 
+              }
+            },
           ),
-        child: AspectRatio(
-          aspectRatio: 16/9,
-          child: _controller.value.isInitialized
-          ? VideoPlayer(_controller)
-          : Container(
-            color: Colors.black,
-          ),
-          ),
+          Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.center,
+                    child:  IconButton(
+                    onPressed: (){
+                      setState(() {
+                        checkVideo();
+                        if(_controller.value.isPlaying){
+                          _controller.pause();
+                        } else{
+                          _controller.play();
+                        }
+                      });
+                    }, 
+                    icon: Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow_sharp)
+                  ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 25,),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          child: Text("00:00"),)
+                      ],
+                    )
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 25,),
+                          Container(
+                            margin: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text("00:00"),)
+                        ],
+                    )
+                  )
+                ]
+              ),
+              VideoProgressIndicator(_controller, allowScrubbing: _isFullWatch),
+            ],
+          )
+        ]
       ),
     );
   }
+
+  BoxDecoration _widgetBorder() {
+    return BoxDecoration(
+      border: _isShowingWidgetOutline
+          ? Border.all(color: Colors.red)
+          : Border.all(color: Colors.transparent),
+    );
+  }
+
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
   }
+
   void checkVideo(){
-    if(_controller.value.position == Duration(seconds: 0, minutes: 0, hours: 0)){
-      print('video Started');
-    }
     if(_controller.value.position == _controller.value.duration) {
-      print('video Ended');
+      _isFullWatch = true;
+      _controller.pause();
     }
   }
 }
-
-
-// class VideoPlayerView extends StatefulWidget {
-//   const VideoPlayerView{Key? key}) : super(key: key);
-
-//   final String title = "Video Player";
-
-//   @override
-//   _VideoPlayerState createState() => _VideoPlayerState();
-// }
-
-// class _VideoPlayerState extends State<VideoPlayer> {
-
-// late VideoPlayerController _controller;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _controller = VideoPlayerController.network('dataSource');
-    
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(child: _controller.value.isInitialized
-//       ? AspectRatio(
-//           aspectRatio: _controller.value.aspectRatio,
-//           child: VideoPlayer(_controller),
-//           )
-//       : Container(),
-//       )
-//     );
-//   }
-// }
